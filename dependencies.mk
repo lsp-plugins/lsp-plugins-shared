2f048aebc1f322b41e9be8d3417c0a946864c318
sadko4u <sadko4u@gmail.com>
2021-01-28 16:48:26 +0300

    Updated build files

--- dependencies.mk
+++ dependencies.mk
@@ -1,3 +1,23 @@
+#
+# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+#
+# This file is part of lsp-plugin-shared
+#
+# lsp-plugin-shared is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# lsp-plugin-shared is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lsp-plugin-shared.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Variables that describe dependencies
 LSP_COMMON_LIB_VERSION     := 1.0.11
 LSP_COMMON_LIB_NAME        := lsp-common-lib
@@ -14,12 +34,13 @@ LSP_PLUGIN_FW_NAME         := lsp-plugin-fw
 LSP_PLUGIN_FW_TYPE         := hdr
 LSP_PLUGIN_FW_URL          := https://github.com/sadko4u/$(LSP_PLUGIN_FW_NAME).git
 
-ifeq ($(PLATFORM),Windows)
-  STDLIB_VERSION             := system
-  STDLIB_TYPE                := opt
-  STDLIB_LDFLAGS             := -lpthread -lshlwapi
-else
-  STDLIB_VERSION             := system
-  STDLIB_TYPE                := opt
-  STDLIB_LDFLAGS             := -lpthread
-endif
+LIBPTHREAD_VERSION         := system
+LIBPTHREAD_NAME            := libpthread
+LIBPTHREAD_TYPE            := opt
+LIBPTHREAD_LDFLAGS         := -lpthread
+
+LIBSHLWAPI_VERSION         := system
+LIBSHLWAPI_NAME            := libshlwapi
+LIBSHLWAPI_TYPE            := opt
+LIBSHLWAPI_LDFLAGS         := -lshlwapi
+
--- project.mk
+++ project.mk
@@ -1,7 +1,27 @@
+#
+# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+#
+# This file is part of lsp-plugin-shared
+#
+# lsp-plugin-shared is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# lsp-plugin-shared is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lsp-plugin-shared.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Package version
+ARTIFACT_ID                 = LSP_PLUGINS_SHARED
 ARTIFACT_NAME               = lsp-plugins-shared
 ARTIFACT_DESC               = Shared resources for LSP Plugins
-ARTIFACT_VARS               = LSP_PLUGINS_SHARED
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_VERSION            = 0.5.0-devel
 ARTIFACT_EXPORT_ALL         = 1
@@ -11,13 +31,19 @@ DEMO_TEST                  := 1
 
 # List of dependencies
 DEPENDENCIES = \
-  STDLIB \
-  LSP_COMMON_LIB \
-  LSP_PLUGIN_FW
+  LIBPTHREAD 
 
 TEST_DEPENDENCIES = \
   LSP_TEST_FW
 
+# Platform-dependent
+ifeq ($(PLATFORM),Windows)
+  TEST_DEPENDENCIES += \
+    LIBSHLWAPI
+endif
+
+# Overall system dependencies
 ALL_DEPENDENCIES = \
   $(DEPENDENCIES) \
-  $(TEST_DEPENDENCIES)
+  $(TEST_DEPENDENCIES) \
+  LIBSHLWAPI
