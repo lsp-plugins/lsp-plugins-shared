ec0b7d4f9ae987f98c581625a8f7bdfb26f241bb
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-21 23:11:39 +0300

    Updated YouTube link

--- src/doc/config/youtube.php
+++ src/doc/config/youtube.php
@@ -23,7 +23,8 @@ $YOUTUBE = array(
 	'g8cShrKtmKo' => 'Crossover',
 	'mEP1WyLFruY' => 'Artistic Delay',
 	'MCIpQebU5o4' => 'Oscilloscope',
-	'6ZuW28u1qns' => 'Multiband Dynamic Processor'
+	'6ZuW28u1qns' => 'Multiband Dynamic Processor',
+	'EFf4VqvMUXM' => 'A/B Tester & Mixer'
 );
 
 ?>
