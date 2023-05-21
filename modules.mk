b9f83221fabf6afda9fd793726d68286ac1acdf5
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-05-21 13:36:06 +0300

    Updated module versions in dependencies

--- modules.mk
+++ modules.mk
@@ -19,19 +19,19 @@
 #
 
 # Variables that describe dependencies
-LSP_COMMON_LIB_VERSION     := 1.0.27
+LSP_COMMON_LIB_VERSION     := 1.0.28
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_TYPE        := src
 LSP_COMMON_LIB_URL_RO      := https://github.com/lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 LSP_COMMON_LIB_URL_RW      := git@github.com:lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 
-LSP_TEST_FW_VERSION        := 1.0.19
+LSP_TEST_FW_VERSION        := 1.0.20
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_TYPE           := src
 LSP_TEST_FW_URL_RO         := https://github.com/lsp-plugins/$(LSP_TEST_FW_NAME).git
 LSP_TEST_FW_URL_RW         := git@github.com:lsp-plugins/$(LSP_TEST_FW_NAME).git
 
-LSP_PLUGIN_FW_VERSION      := 1.0.11
+LSP_PLUGIN_FW_VERSION      := 1.0.12
 LSP_PLUGIN_FW_NAME         := lsp-plugin-fw
 LSP_PLUGIN_FW_TYPE         := src
 LSP_PLUGIN_FW_URL_RO       := https://github.com/lsp-plugins/$(LSP_PLUGIN_FW_NAME).git
