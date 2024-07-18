398c4e591ab86683510e549eecfdf4c8070716d3
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-07-18 22:50:52 +0300

    Updated plugin documentation

--- src/doc/inc/menu.php
+++ src/doc/inc/menu.php
@@ -1,16 +1,30 @@
 <?php
-	$MENU = array( );
+	$MENU = [
+		'id' => '',
+		'parent' => null,
+		'text' => '',
+		'root' => '',
+		'path' => '',
+		'style' => null,
+		'file' => ''
+	];
 	
 	function &find_menu_item($id, &$menu = null)
 	{
-		$result = null;
 		global $MENU;
-		if (!isset($id))
-			return $result;
-		if (!isset($menu))
-			$menu = &$MENU;
 		
-		foreach ($menu as &$item)
+		$result = null;
+		if (!isset($id)) {
+			return $result;
+		}
+		if (!isset($menu)) {
+			$menu = &$MENU;
+		}
+		if (!array_key_exists('items', $menu)) {
+			return $result;
+		}
+		
+		foreach ($menu['items'] as &$item)
 		{
 			if (isset($item['id']) && ($item['id'] == $id))
 				return $item;
@@ -26,7 +40,7 @@
 		return $result;
 	}
 
-	function def_menu_item($parent, $id, $text, $file = null)
+	function def_menu_item($parent, $id, $text, $css_style, $file = null)
 	{
 		global $MENU;
 		
@@ -36,45 +50,54 @@
 			'text' => $text,
 			'root' => '',
 			'path' => '',
+			'style' => $css_style,
 			'file' => isset($file) ? $file : $id
 		);
 
 		if ((!isset($parent)) || (strlen($parent) <= 0))
 		{
-		    $item['root']    = '..';
-			array_push($MENU, $item);
+			$item['root']    = '..';
+			$mi = &$MENU;
 		}
 		else
 		{
 			$mi = &find_menu_item($parent);
 			$item['path']   .= ((strlen($mi['path']) > 0) ? $mi['path'] . '/' : '') . $mi['id'];
 			$item['root']   .= '..' . ((strlen($mi['root']) > 0) ? '/' . $mi['root'] : '');
-			
-			if (!isset($mi['items']))
-				$mi['items'] = array( $item );
-			else
-				array_push($mi['items'], $item);
 		}
+		
+		if ((!array_key_exists('items', $mi)) || (!isset($mi['items'])))
+			$mi['items'] = [ $item ];
+		else
+			array_push($mi['items'], $item);
 	}
 	
 	function output_menu(&$menu = null)
 	{
 		global $MENU;
-		if (!isset($menu))
+		if (!isset($menu)) {
 			$menu = &$MENU;
+		}
 		
-		print "<ul class=\"index\">\n";
-		foreach ($menu as &$item)
-		{
-			if (isset($item['items']))
+		$css_class = "index";
+		if (isset($menu['style'])) {
+			$css_class .= " " . $menu['style'];
+		}
+		
+		print "<ul class=\"{$css_class}\">\n";
+		if (array_key_exists('items', $menu)) {
+			foreach ($menu['items'] as &$item)
 			{
-				print("<li>{$item['text']}</li>\n");
-				output_menu($item['items']);
-			}
-			else
-			{
-				$path = (strlen($item['path']) > 0) ? $item['path'] . '/' : '';
-				print("<li><a href=\"html/{$path}{$item['id']}.html\">{$item['text']}</a></li>\n");
+				if (array_key_exists('items', $item))
+				{
+					print("<li>{$item['text']}</li>\n");
+					output_menu($item);
+				}
+				else
+				{
+					$path = (strlen($item['path']) > 0) ? $item['path'] . '/' : '';
+					print("<li><a href=\"html/{$path}{$item['id']}.html\">{$item['text']}</a></li>\n");
+				}
 			}
 		}
 		print "</ul>\n";
@@ -83,15 +106,19 @@
 	function raw_menu_list(&$target, &$menu = null)
 	{
 		global $MENU;
-		if (!isset($menu))
+		if (!isset($menu)) {
 			$menu = &$MENU;
+		}
 		
-		foreach ($menu as &$item)
-		{
-			if (isset($item['items']))
-				raw_menu_list($target, $item['items']);
-			else
-				array_push($target, $item);
+		if (array_key_exists('items', $menu)) {
+			foreach ($menu['items'] as &$item)
+			{
+				if (array_key_exists('items', $item)) {
+					raw_menu_list($target, $item['items']);
+				} else {
+					array_push($target, $item);
+				}
+			}
 		}
 	}
 
