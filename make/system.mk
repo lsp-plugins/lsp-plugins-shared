30f1345e026814965b8b0a52176f4562a568a734
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-09-14 20:32:23 +0300

    Updated makefiles

--- make/system.mk
+++ make/system.mk
@@ -54,7 +54,15 @@ endif
 
 # Set actual architecture
 # The current architecture can be obtained by: gcc -Q --help=target
-ifeq ($(patsubst armv6%,armv6,$(BUILD_ARCH)),armv6)
+ifeq ($(BUILD_ARCH),armel)
+  override ARCHITECTURE   = $(BUILD_ARCH)
+  ARCHITECTURE_FAMILY     = generic
+  ARCHITECTURE_CFLAGS    :=
+else ifeq ($(BUILD_ARCH),armhf)
+  override ARCHITECTURE   = arm32
+  ARCHITECTURE_FAMILY     = arm32
+  ARCHITECTURE_CFLAGS    := -march=armv7-a+fp -marm
+else ifeq ($(patsubst armv6%,armv6,$(BUILD_ARCH)),armv6)
   override ARCHITECTURE   = arm32
   ARCHITECTURE_FAMILY     = arm32
   ARCHITECTURE_CFLAGS    := -march=armv6 -marm
@@ -67,9 +75,9 @@ else ifeq ($(patsubst armv7%,armv7,$(BUILD_ARCH)),armv7)
   ARCHITECTURE_FAMILY     = arm32
   ARCHITECTURE_CFLAGS    := -march=armv7-a -marm
 else ifeq ($(patsubst armv8%,armv8,$(BUILD_ARCH)),armv8)
-  override ARCHITECTURE   = aarch64
-  ARCHITECTURE_FAMILY     = aarch64
-  ARCHITECTURE_CFLAGS    := -march=armv8-a
+  override ARCHITECTURE   = arm32
+  ARCHITECTURE_FAMILY     = arm32
+  ARCHITECTURE_CFLAGS    := -march=armv7-a -marm
 else ifeq ($(patsubst aarch64%,aarch64,$(BUILD_ARCH)),aarch64)
   override ARCHITECTURE   = aarch64
   ARCHITECTURE_FAMILY     = aarch64
@@ -111,9 +119,9 @@ else ifeq ($(patsubst i%86,i586,$(BUILD_ARCH)),i586)
   ARCHITECTURE_FAMILY     = ia32
   ARCHITECTURE_CFLAGS    := -march=i586 -m32
 else ifeq ($(BUILD_ARCH),x86)
-  override ARCHITECTURE   = i586
+  override ARCHITECTURE   = i686
   ARCHITECTURE_FAMILY     = ia32
-  ARCHITECTURE_CFLAGS    := -march=i586 -m32
+  ARCHITECTURE_CFLAGS    := -march=i686 -m32
 else ifeq ($(BUILD_ARCH),riscv32)
   override ARCHITECTURE   = riscv32
   ARCHITECTURE_FAMILY     = riscv32
@@ -165,39 +173,6 @@ ifndef PKGCONFIG_EXT
   PKGCONFIG_EXT            := .pc
 endif
 
-# Installation prefix
-ifndef PREFIX
-  ifeq ($(PLATFORM),Windows)
-    PREFIX                   := $(ProgramFiles)
-  else
-    PREFIX                   := /usr/local
-  endif
-endif
-
-# Library prefix
-ifndef LIBDIR
-  LIBDIR                   := $(PREFIX)/lib
-endif
-
-# Binaries prefix
-ifndef BINDIR
-  BINDIR                   := $(PREFIX)/bin
-endif
-
-# Binaries prefix
-ifndef INCDIR
-  INCDIR                   := $(PREFIX)/include
-endif
-
-# Temporary directory
-ifndef TEMPDIR
-  ifeq ($(PLATFORM),Windows)
-    TEMPDIR                  := $(TEMP)
-  else
-    TEMPDIR                  := /tmp
-  endif
-endif
-
 TEST                       := 0
 
 # Set-up list of common variables
@@ -205,27 +180,20 @@ COMMON_VARS = \
 	ARCHITECTURE \
 	ARCHITECTURE_FAMILY \
 	ARCHITECTURE_CFLAGS \
-	BINDIR \
-	BUILDDIR \
+	CROSS_COMPILE \
 	DEBUG \
-	ETCDIR \
 	EXECUTABLE_EXT \
 	EXPORT_SYMBOLS \
 	FEATURES \
-	INCDIR \
 	INSTALL_HEADERS \
-	LIBDIR \
 	LIBRARY_EXT \
 	LIBRARY_PREFIX \
 	PKGCONFIG_EXT \
 	PLATFORM \
-	PREFIX \
-	ROOTDIR \
 	ROOT_ARTIFACT_ID \
 	PROFILE \
-	SHAREDDIR \
 	STATICLIB_EXT \
-	TEMPDIR \
+	STRICT \
 	TEST \
 	TRACE
 
@@ -238,25 +206,21 @@ sysvars:
 	echo "  ARCHITECTURE_CFLAGS       compiler flags to specify architecture"
 	echo "  ARCHITECTURE_FAMILY       compiler flags to specify architecture family"
 	echo "  ARCHITECTURE_LDFLAGS      linker flags to specify architecture"
-	echo "  BINDIR                    location of the binaries"
-	echo "  BUILDDIR                  location of the build directory"
+	echo "  CROSS_COMPILE             enable/disable cross-compilation"
 	echo "  DEBUG                     build with debug options"
 	echo "  DEVEL                     build with modules checked out for read/write URL"
-	echo "  ETCDIR                    location of system configuration files"
 	echo "  EXECUTABLE_EXT            file extension for executable files"
+	echo "  EXPORT_SYMBOLS            make export symbols visible" 
 	echo "  FEATURES                  list of features enabled in the build"
-	echo "  INCDIR                    location of the header files"
-	echo "  LIBDIR                    location of the library"
+	echo "  INSTALL_HEADERS           install headers (enabled by default)"
 	echo "  LIBRARY_EXT               file extension for library files"
 	echo "  LIBRARY_PREFIX            prefix used for library file"
 	echo "  PKGCONFIG_EXT             file extension for pkgconfig files"
 	echo "  PLATFORM                  target software platform to perform build"
-	echo "  PREFIX                    installation prefix for binary files"
 	echo "  PROFILE                   build with profile options"
-	echo "  SHAREDDIR                 location of the shared files"
 	echo "  STATICLIB_EXT             file extension for static library files"
+	echo "  STRICT                    strict compilation: treat compilation warnings as errors"
 	echo "  SUB_FEATURES              list of features disabled in the build as a subtraction of default"
-	echo "  TEMPDIR                   location of temporary directory"
 	echo "  TEST                      use test build"
 	echo "  TRACE                     compile with additional trace information output"
 
