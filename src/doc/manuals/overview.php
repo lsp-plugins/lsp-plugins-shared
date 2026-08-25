28d3f34ee8dd622f17d1f90db2bf9910279dcbfc
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-08-26 00:05:08 +0300

    Updated overview in the documentation

--- src/doc/manuals/overview.php
+++ src/doc/manuals/overview.php
@@ -1,12 +1,13 @@
 <p><img src="<?= $DOCROOT ?>img/box.png" class="bundle-box" ></p>
 <p><b>LSP (Linux Studio Plugins)</b> is a collection of open-source plugins compatible with following plugin formats:</p>
 <ul>
-	<li><b>CLAP</b> - set of plugins for <a href="https://github.com/free-audio/clap">Clever Audio Plugins API</a></li>
-	<li><b>JACK</b> - Standalone versions for <a href="https://jackaudio.org/">JACK Audio connection Kit</a> with UI</li>
-	<li><b>LADSPA</b> - set of plugins for <a href="https://en.wikipedia.org/wiki/LADSPA">Linux Audio Developer's Simple Plugin API</a></li>
-	<li><b>LV2</b> - set of plugins and UIs for <a href="https://lv2plug.in/">Linux Audio Developer's Simple Plugin API (LADSPA) version 2</a></li>
-	<li><b>VST2 / LinuxVST</b> - set of plugins for <a href="https://www.steinberg.net/">Steinberg's VST 2.4</a> plugin format</li>
-	<li><b>VST3</b> - set of plugins and for <a href="https://www.steinberg.net/">Steinberg's VST 3</a> plugin format</li>
+	<li><b>CLAP</b> - set of plugins for <a href="https://github.com/free-audio/clap">Clever Audio Plugins API</a>.</li>
+	<li><b>GStreamer</b> - set of plugins for <a href="https://gstreamer.freedesktop.org/">GStreamer Multimedia Framework</a>.</li>
+	<li><b>LADSPA</b> - set of plugins for <a href="https://en.wikipedia.org/wiki/LADSPA">Linux Audio Developer's Simple Plugin API</a>.</li>
+	<li><b>LV2</b> - set of plugins and UIs for <a href="https://lv2plug.in/">Linux Audio Developer's Simple Plugin API (LADSPA) version 2</a>.</li>
+	<li><b>Standalone</b> - Standalone versions for <a href="https://jackaudio.org/">JACK Audio connection Kit</a> and <a href="https://pipewire.org/">PipeWire Multimedia Server</a> with UI.</li>
+	<li><b>VST2 / LinuxVST</b> - set of plugins for <a href="https://www.steinberg.net/">Steinberg's VST 2.4</a> plugin format.</li>
+	<li><b>VST3</b> - set of plugins and for <a href="https://www.steinberg.net/">Steinberg's VST 3</a> plugin format.</li>
 </ul>
 
 <p>The basic idea is to fill the lack of good and useful plugins under the GNU/Linux platform.</p>
